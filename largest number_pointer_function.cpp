e5d5390e8f269b835f5e1c7e48a45a6ba419d4b6
Indranil65090 <63942627+Indranil65090@users.noreply.github.com>
2020-04-30 13:56:14 +0530

    Add files via upload

---
+++ largest number_pointer_function.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int great(int *,int *,int *,int *);
+int main()
+{
+	int num1,num2,num3,num4;
+	printf("Enter the 1st number:");
+	scanf("%d",&num1);
+	printf("Enter the 2nd number:");
+	scanf("%d",&num2);
+	printf("Enter the 3rd number:");
+	scanf("%d",&num3);
+	great(&num1,&num2,&num3,&num4);
+	printf("The largest number is:%d",num4);
+	return 0;
+}
+int great(int *a,int *b,int *c,int *large)
+{
+	if(*a>*b && *a>*c)
+	*large=*a;
+	else if(*b>*a && *b>*c)
+	*large=*b;
+	else
+	*large=*c;
+}
