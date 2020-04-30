435c4c384a8e50b9c6f3b423ab3559c8af5f31a2
Indranil65090 <63942627+Indranil65090@users.noreply.github.com>
2020-04-30 13:57:20 +0530

    Add files via upload

---
+++ half star pattern.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,r;
+	printf("Enter the number of rows:");
+	scanf("%d",&n);
+	for(r=1;r<=n;r++)
+	{
+		for(i=1;i<=r;i++)
+		{
+			printf("*");
+		}
+		printf("\n");	
+	}
+	for(r=(n-1);r>=1;r--)
+	{
+		for(i=r;i>=1;i--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
