668ed142bd24c434c7521fe06e75c86c7fca6f63
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-01 00:47:19 +0530

    Update and rename half star pattern.cpp to Half diamond.cpp

--- Half diamond.cpp
+++ Half diamond.cpp
@@ -1,9 +1,14 @@
+/**
+half diamond pattern
+*/
 #include<stdio.h>
 int main()
 {
 	int i,n,r;
+	
 	printf("Enter the number of rows:");
 	scanf("%d",&n);
+	
 	for(r=1;r<=n;r++)
 	{
 		for(i=1;i<=r;i++)
@@ -12,6 +17,7 @@ int main()
 		}
 		printf("\n");	
 	}
+	
 	for(r=(n-1);r>=1;r--)
 	{
 		for(i=r;i>=1;i--)
@@ -20,5 +26,6 @@ int main()
 		}
 		printf("\n");
 	}
+	
 	return 0;
 }
