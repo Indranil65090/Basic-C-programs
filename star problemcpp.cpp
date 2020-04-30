82b9c34281f1251bdc7f361298d2e1298bbe7e34
Indranil65090 <63942627+Indranil65090@users.noreply.github.com>
2020-04-30 13:37:25 +0530

    Add files via upload

---
+++ star problemcpp.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int rows,space,i,j;
+	printf("Enter the rows:");
+	scanf("%d",&rows);
+	for(i=rows;i>=1;i--)
+	{
+		for(space=i;space<rows;space++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=(2*i-1);j++)                         
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
