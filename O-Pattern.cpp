da3142bd008ee419f3914a2b55c826b6c35d7197
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-01 00:22:32 +0530

    Update and rename O pattern.cpp to O-Pattern.cpp

--- O-Pattern.cpp
+++ O-Pattern.cpp
@@ -1,9 +1,14 @@
+/**
+creating an o-pattern incrementing in the even order
+*/
 #include<stdio.h>
 int main()
 {
 	int n,i,j;
-	printf("Enter the number of rows:\n");
+	
+	printf("Enter the number of rows:");
 	scanf("%d",&n);
+	
 	for(i=1;i<=n;i++)
 	{
 	  for(j=1;j<=2*i;j++)
@@ -12,5 +17,6 @@ int main()
 	  }  
 	printf("\n");
     }
+	
 	return 0;
 }
