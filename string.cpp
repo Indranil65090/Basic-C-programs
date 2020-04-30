493f9ab18c91a6de2f29a6591f04c3e5cd333e62
Indranil65090 <63942627+Indranil65090@users.noreply.github.com>
2020-04-30 13:44:33 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,rows;
+	char alphabet='A';
+	printf("Enter the rows:");
+	scanf("%d",&rows);
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%c",alphabet);
+		}
+		++alphabet;
+		printf("\n");
+	}
+	return 0;
+}
