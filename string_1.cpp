23dd9f9bf0f0fe0f6120b67fdd45c1868d19409f
Indranil65090 <63942627+Indranil65090@users.noreply.github.com>
2020-04-30 13:41:26 +0530

    Add files via upload

---
+++ string_1.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i;
+	char str[80],ch,newch;
+	
+	printf("Enter the string:\n");
+	gets(str);
+	
+	printf("Enter the character to be replaced:\n");
+	scanf("%c",&ch);
+	
+	getchar();
+
+	printf("Enter the number with which it is replaced:\n");
+	scanf("%c",&newch);
+	
+	
+	for(i=0;i<strlen(str);i++)
+	{
+		if(str[i]==ch)
+		{
+			str[i]=newch;
+		}
+	}
+	printf("The new string is replaced '%c' with '%c'=%s",ch,newch,str);
+	return 0;
+}
