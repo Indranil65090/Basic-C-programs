c99abf2810bdda1a2d69c74f19304723fa910077
Indranil65090 <63942627+Indranil65090@users.noreply.github.com>
2020-04-30 14:41:04 +0530

    Add files via upload

---
+++ Pattern_004.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int r,i,n;
+	printf("Enter the number of rows:");
+	scanf("%d",&n);
+	for(r=n;r>=1;r--)
+	{
+		char ch ='A';
+		for(i=1;i<=r;i++)
+		{
+			printf("%c",ch);
+			ch=ch+1;
+		}
+		for(int space=1;space<=(2*n-2*r);space++)
+		{
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
+
+
