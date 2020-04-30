c2454cae75d7207c97bbee59900bfec9e7da3faf
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-01 00:12:00 +0530

    Update Evil Number.cpp

--- Evil Number.cpp
+++ Evil Number.cpp
@@ -1,19 +1,22 @@
+/**
+An evil number is a non-negative number that has an even number of 1s in its binary expansion.
+*/
 #include<stdio.h>
 int main()
 {
 	int c,n,count=0;
 	printf("Enter the number from user:");
-	scanf("%d",&n);
+	scanf("%d",&n);//taking the input from user
 	while(n>0)
 	{
 		c=n%2;
 		n=n/2;
-		if(c==1)
+		if(c==1)//checking if the remainder is equal to 1s or not
 		{
-			count++;
+			count++;//counting the number of 1
 		}
 	}
-	if(count%2==0)
+	if(count%2==0)//checking if the number of 1s is even or not
 		{
 			printf("This is an evil number");
 		}
