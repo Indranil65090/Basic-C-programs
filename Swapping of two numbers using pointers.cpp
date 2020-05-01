ae1766997b0eed55d88582c9cf84da6f2c7d798b
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-01 10:20:42 +0530

    Update and rename pointer_3rd_program.cpp to Swapping of two numbers using pointers.cpp

--- Swapping of two numbers using pointers.cpp
+++ Swapping of two numbers using pointers.cpp
@@ -3,16 +3,17 @@ int main()
 {
 	int a,b;
 	int *aa,*bb;
+	/*storing the address of the two numbers*/
 	aa=&a;
 	bb=&b;
-	
+	/*entering the two numbers*/
 	printf("Enter the two numbers:\n");
 	scanf("%d %d",aa,bb);
-	
+	/*swapping of two numbers*/
 	*aa=(*aa)+(*bb);
 	*bb=(*aa)-(*bb);
 	*aa=(*aa)-(*bb);
-	
+	/*printing the two numbers*/
 	printf("The two numbers are:\n%d %d",*aa,*bb);
 	
 	return 0;
