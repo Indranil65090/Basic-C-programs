52d0da4d342ed939f7cabfe6b0401c7974edcd34
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-01 10:05:48 +0530

    Update and rename new_reverse_ABC.cpp to Pattern_006.cpp

--- Pattern_006.cpp
+++ Pattern_006.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int i,j,n;
+	int rows,j,n;
 	printf("Enter the number of rows:");
 	scanf("%d",&n);
 	
-	for(i=1;i<=n;i++)
+	for(rows=1;rows<=n;rows++)
 	{
-		for(j=i;j>=1;j--)
+		for(j=rows;j>=1;j--)
 		{
-				printf("%c",'A'+j-1);
+			printf("%c",'A'+j-1);
 		}
 		printf("\n");
 	}
