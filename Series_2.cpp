3e092e3e8051e2f577b72f32f6db14c8c27af6d1
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-01 10:36:02 +0530

    Update and rename series_2.cpp to Series_2.cpp

--- Series_2.cpp
+++ Series_2.cpp
@@ -4,13 +4,16 @@ int main()
 {
 	int t,n;
 	float y;
-	printf("Enter the number of iterations the user want:");
+	
+	printf("Enter the value of n");
 	scanf("%d",&n);
+	
 	for(t=0;t<n;t++)
 	{
 		y=2*pow(2.1782,-0.1*t)*sin(0.5*t);
-		printf("y=%3f",y);
+		printf("y=%.3f",y);
 		printf("\n");
 	}
 	
+	return 0;
 }
