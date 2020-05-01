236616ca5cb3d5d4d0d114dce7ff56d0adeef1cd
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-05-01 10:16:54 +0530

    Update and rename pointer and array largest number.cpp to Largest number using pointer and array.cpp

--- Largest number using pointer and array.cpp
+++ Largest number using pointer and array.cpp
@@ -1,20 +1,27 @@
+/**
+finding the largest number in the array using pointer
+*/
 #include<stdio.h>
 int main()
 {
 	int i,n,a[50],pos,large=-1;
 	int *pn,*pnum,*ppos,*plarge;
-	pnum=a;
+	
+	pnum=a;//same as &a[0]
 	plarge=&large;
 	pn=&n;
 	ppos=&pos;
+	
 	printf("Enter the number of elements of the array:");
 	scanf("%d",pn);
-	for(i=0;i<*pn;i++)
+	
+	for(i=0;i<*pn;i++)//entering the elements of the array
 	{
 		printf("a[%d]=",i);
 		scanf("%d",pnum+i);
 	}
-	for(i=0;i<*pn;i++)
+	
+	for(i=0;i<*pn;i++)//finding the largest number of the array
 	{
 		if(*(pnum+i)>*plarge)
 		{
@@ -22,14 +29,17 @@ int main()
 			*ppos=i;
 		}
 	}
+	
 	printf("The largest number of the array is:%d\n",*plarge);
 	printf("The position of the largest value is:%d\n",*ppos);
+	
 	for(i=0;i<*pn;i++)
 	{
 		printf("a[%d]=",i);
 		printf("%d",*(pnum+i));
 		printf("\n");
 	}
+	
 	return 0;
 }
 	
