a89eea416d813b1a044a5fbd5b4accb06ae23eeb
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-06-15 18:05:04 +0530

    Create L.C.M of two numbers using G.C.D and recursion.c

---
+++ L.C.M of two numbers using G.C.D and recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>  
+int gcd(int a, int b)  
+{  
+    if(a==0)
+	{
+		return b;  
+	} 
+    return gcd(b % a, a);  
+}  
+int lcm(int a, int b)  
+{  
+    return (a*b)/gcd(a,b);  
+}  
+int main()  
+{  
+    int a,b;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+    printf("LCM of %d and %d is:%d",a,b,lcm(a, b));  
+    return 0;  
+}  
