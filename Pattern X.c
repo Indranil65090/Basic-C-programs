2688dbb2c28919f981fdbd0c0f8934b85d75ee08
Indranil Dutta <63942627+Indranil65090@users.noreply.github.com>
2020-07-14 18:18:17 +0530

    Create Pattern X.c

---
+++ Pattern X.c
@@ -0,0 +1,29 @@
+/**
+ * C program to print X star pattern series
+ */
+ 
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+    int count;
+	printf("Enter the number of rows: ");
+    scanf("%d", &n);
+	count = 2 * n - 1;
+	for(i=1; i<=count; i++)
+    {
+        for(j=1; j<=count; j++)
+        {
+            if(j==i || (j==count - i + 1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+		printf("\n");
+    }
+	return 0;
+}
